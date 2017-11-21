b257249c187cfb7bd850dc7bb6f7486716c30a99
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-20 20:17:23 -0500

    stopping point for class

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-class 
+class Fighter {
+  constuctor(name,location, weightclass, title, weapon, specialMove) {
+    this.name = name;
+    this.location = location;
+    this.weightclass = 170;
+    this.title = true;
+    this.weapon = weapon;
+    this.specialMove = move;
+  }
+
+  name(
+    console.log(this.name);
+  )
+
+  fight() {
+    console.log(this.weapon);
+  }
+
+  result() {
+    console.log("Winner")
+  }
+
+}
+
+var fighter = new Fighter("Asher Dasher", "Heaven", "Angelweight", "Ultimate Wings", ["Speed","Song"], "Cupid's Arrow" )
